676ba185a2173592b7da0f614485a08c3f4c67a4
Thiago Delmotte <thiago.delmotte@gmail.com>
2019-07-04 17:04:13 -0300

    initial project

---
+++ project/README.md
@@ -0,0 +1,40 @@
+# Breiv (iOS App)
+
+- Author: Thiago Delmotte (thiago.delmotte@gmail.com)
+
+## Description
+
+- MVVM Architecture
+- Swift 5
+- StyleGuide
+- i18n
+- Animations
+- Error handling no internet connection
+- Encapsulation of third-party frameworks for easy maintenance
+- View Code
+- Endpoint in the Info.plist
+- Alamofire
+- Codable Protocol
+- Custom Fonts
+- Unit/UI Tests
+
+## Requirements
+
+- Xcode 10.2.1+
+- iOS 11.0+
+- Swift 5+
+- CocoaPods
+
+### CocoaPods
+
+[CocoaPods](http://cocoapods.org) is a dependency manager for Cocoa projects. You can install it with the following command:
+
+```bash
+$ gem install cocoapods
+```
+
+After that, go to project directory and run:
+
+```bash
+$ pod install
+```
---
+++ project/breivTests/breivTests.swift
@@ -0,0 +1,34 @@
+//
+//  breivTests.swift
+//  breivTests
+//
+//  Created by Thiago Delmotte on 04/07/19.
+//  Copyright © 2019 breiv. All rights reserved.
+//
+
+import XCTest
+@testable import breiv
+
+class breivTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
