b615ea87a466b557e4726e42653066038d011e36
Thiago Delmotte <thiago.delmotte@gmail.com>
2019-07-04 18:37:53 -0300

    network layer

---
+++ project/breiv/Extensions/Bundle.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+extension Bundle {
+    
+    var endpointBase: URL {
+        let endpoint = self.object(forInfoDictionaryKey: "ENDPOINT_BASE") as! String
+        return URL(string: endpoint)!
+    }
+    
+    var endpointConnection: URL {
+        let endpoint = self.object(forInfoDictionaryKey: "ENDPOINT_CONNECTION") as! String
+        return URL(string: endpoint)!
+    }
+    
+}
